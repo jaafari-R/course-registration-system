b4e7c48dbfe18c19407b5b1f8d0b7659720671b8
Ramadan Jaafari <58656241+jaafari-R@users.noreply.github.com>
2023-05-08 22:54:49 +0000

    Created Student Register frontend

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World'
+    return render_template('./register.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    print(request.form)
+    return "ok"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ model/course_registration_model.py
@@ -0,0 +1,25 @@
+from db_connect import db_connection
+
+class CourseRegisterModel:
+    def __init__(self):
+        pass
+
+    # TODO
+    def create_user(self):
+        pass
+
+    # TODO
+    def get_user(self):
+        pass
+
+    # TODO
+    def create_course(self):
+        pass
+
+    # TODO
+    def get_courses(self):
+        pass
+
+    # TODO
+    def search_courses(self, course_code, course_name, course_instructor):
+        pass
