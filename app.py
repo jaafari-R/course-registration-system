48ef8e59e0acc376dfebf9b08f788cf779941482
Ramadan Jaafari <58656241+jaafari-R@users.noreply.github.com>
2023-05-09 01:07:11 +0000

    Implemented FrontEnd User Signin

--- app.py
+++ app.py
@@ -9,14 +9,27 @@ course_reg_controller = CourseRegisterationController(course_reg_model)
 
 @app.route('/')
 def index():
+    return 'Hello'
+
+@app.route('/signup', methods=['GET'])
+def register_form():
     return render_template('./register.html')
 
-@app.route('/register', methods=['POST'])
+@app.route('/signup', methods=['POST'])
 def register():
     res = course_reg_controller.register_student(request.form)
     if(res != 'success'):
         return res
     return "ok"
+
+@app.route('/signin', methods=['GET'])
+def login_form():
+    return render_template('./login.html')
+
+@app.route('/signin', methods=['POST'])
+def login():
+    return 'ok'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
