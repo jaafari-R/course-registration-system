2485c9bd9612a7930dd8955da2973422feaf6fd9
Ramadan Jaafari <58656241+jaafari-R@users.noreply.github.com>
2023-05-09 12:18:41 +0000

    Implemented redirection for signin and signup based on session

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ course_reg_model = CourseRegisterationModel()
 course_reg_controller = CourseRegisterationController(course_reg_model)
 
 
+
+
 @app.route('/')
 def index():
     cookie = request.cookies.get('course_reg')
@@ -16,28 +18,47 @@ def index():
 
     return 'Hello'
 
+
 @app.route('/signup', methods=['GET'])
 def register_form():
+    cookie = request.cookies.get('course_reg')
+    if course_reg_controller.verifySession(cookie):
+        return redirect(url_for('index'))
+
     return render_template('./register.html')
+
+
+@app.route('/signin', methods=['GET'])
+def login_form():
+    cookie = request.cookies.get('course_reg')
+    if course_reg_controller.verifySession(cookie):
+        return redirect(url_for('index'))
+    return render_template('./login.html')
+
 
 @app.route('/signup', methods=['POST'])
 def register():
     status, cookie, exp_date = course_reg_controller.register_student(request.form)
-    res = make_response(cookie)
-    if(status == 'success'):
-        res.set_cookie('course_reg', value=cookie, expires=exp_date)
-    return res
 
-@app.route('/signin', methods=['GET'])
-def login_form():
-    return render_template('./login.html')
+    # failed to login
+    if(status == 'fail'):
+        return cookie
+    
+    res = make_response(redirect(url_for('index')))
+    res.set_cookie('course_reg', value=cookie, expires=exp_date)
+    return res
+    
 
 @app.route('/signin', methods=['POST'])
 def login():
     status, cookie, exp_date = course_reg_controller.login(request.form)
-    res = make_response(cookie)
-    if(status == 'success'):
-        res.set_cookie('course_reg', value=cookie, expires=exp_date)
+    
+    # failed to login
+    if(status == 'fail'):
+        return cookie
+    
+    res = make_response(redirect(url_for('index')))
+    res.set_cookie('course_reg', value=cookie, expires=exp_date)
     return res
 
 
